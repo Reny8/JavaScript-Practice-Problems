feaa100499ed40e121989d6338812668c8e83518
Reny Reyes <renyreyes8@hotmail.com>
2022-04-12 12:23:15 -0400

    Completed meals problem 3

--- worksheet.js
+++ worksheet.js
@@ -1,7 +1,7 @@
 //DAYS OF THE WEEK
-let dayOfWeek = 'Monday';
-console.log(dayOfWeek);
-dayOfWeek = 'Friday';
+let dayOfWeek = 'Monday'
+console.log(dayOfWeek)
+dayOfWeek = 'Friday'
 // String interpolation
 console.log(`I can't wait for ${dayOfWeek}!`)
 
@@ -9,3 +9,23 @@ console.log(`I can't wait for ${dayOfWeek}!`)
 let animalInput = prompt('What is your favorite animal? ')
 let colorInput = prompt('What is your favorite color? ')
 console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+//CONDITIONALS
+// Favorite Breakfast: French Toast 
+// Favorite Lunch: Birra Tacos
+// Favorite Dinner: Geneitos
+let timeOfDay = 1800
+let favoriteFood = null
+
+if (timeOfDay < 1200) {
+    favoriteFood = "French Toast";
+    console.log(favoriteFood)
+}
+if (timeOfDay > 1200 & timeOfDay < 1700) {
+    favoriteFood = "Birra Tacos";
+    console.log(favoriteFood)
+}
+if (timeOfDay > 1700) {
+    favoriteFood = "Geneitos";
+    console.log(favoriteFood)
+}
