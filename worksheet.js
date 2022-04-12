ab16dedc4397e871e48a28a12b7f28c8bc7c9411
Reny Reyes <renyreyes8@hotmail.com>
2022-04-12 11:50:27 -0400

    Completed User Input problem 2 + Tested

--- worksheet.js
+++ worksheet.js
@@ -4,3 +4,8 @@ console.log(dayOfWeek);
 dayOfWeek = 'Friday';
 // String interpolation
 console.log(`I can't wait for ${dayOfWeek}!`)
+
+// USER INPUT 
+let animalInput = prompt('What is your favorite animal? ')
+let colorInput = prompt('What is your favorite color? ')
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
