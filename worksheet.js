728ee7619abb1eee41820abfb8b345670bc972f9
Reny Reyes <renyreyes8@hotmail.com>
2022-04-12 15:07:52 -0400

    Finished the rest of the problems + Tested all

--- worksheet.js
+++ worksheet.js
@@ -36,13 +36,13 @@ let randomNumber = Math.floor(Math.random() * 10)
 if (randomNumber > 0 & randomNumber < 2) {
     console.log('Beatles')
 }
-if (randomNumber > 3 & randomNumber < 5) {
+else if (randomNumber > 3 & randomNumber < 5) {
     console.log('Stones')
 }
-if (randomNumber > 6 & randomNumber < 8) {
+else if (randomNumber > 6 & randomNumber < 8) {
     console.log('Floyd')
 }
-if (randomNumber == 9 || randomNumber == 10) {
+else if (randomNumber == 9 || randomNumber == 10) {
     console.log('Hendrix')
 }
 
@@ -64,3 +64,66 @@ for (let greeting = 0; greeting < 5; greeting++) {
     console.log('hello')
     console.log('goodbye')
 }
+
+// FAVORITE MOVIE(VOID FUNCTIONS)
+
+function printMovieName() {
+    let favoriteMovie = 'Interstellar'
+    console.log(favoriteMovie)
+}
+printMovieName()
+
+//FAVORITE BAND (RETURN FUNCTIONS)
+
+function favoriteBand() {
+    let userInput = prompt("Enter your favorite band: ")
+    return userInput
+}
+band = favoriteBand()
+console.log(band)
+
+//CONCERT PARAMETERS 
+
+function concertDisplay(musicalArt) {
+    let myStreet = prompt("Enter the street you live on: ")
+    console.log(`It would be great if ${musicalArt} played a show on ${myStreet}!`)
+}
+concertDisplay('My Chemical Romance')
+
+//ARRAYS 
+
+let desktopItems = ['water bottle','cellphone','laptop','tablet']
+console.log(desktopItems[1])
+desktopItems.push('Infinity Gauntlet')
+for (let item in desktopItems) {
+    console.log(desktopItems[item])
+}
+
+//MAGIC NUMBER 
+
+let magicNumber = Math.floor(Math.random() * 100)
+let guess = 0
+let correctAnswer = false
+
+
+while (correctAnswer == false) {
+    number = prompt("Guess the magic number: ");
+    if ( magicNumber - 10 == number || magicNumber + 10 == number) {
+        console.log("Getting Warmer");
+        guess++
+    } 
+    else if ( number > magicNumber ) {
+        console.log("Too high");
+        guess++
+    }
+    else if ( number < magicNumber ) {
+        console.log("Too Low");
+        guess++
+    }
+    else if ( number == magicNumber ) {
+        console.log(`You guessed ${number} which is correct!`);
+        console.log(`Times guessed: ${guess}`);
+        correctAnswer = true;
+    }
+}
+
